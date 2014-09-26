fb054c8ea482823a3a45a0fc5e6f5cd8845803a4
Chelsea <chelsea.bailey@students.olin.edu>
2014-09-26 11:29:21 -0400

    initial site files

---
+++ js/actions.js
@@ -0,0 +1,24 @@
+$(document).ready(function()  {
+
+	var duration = 1000;
+	var $root = $('html, body');
+	var checkPoint = $('#nav').offset().top;
+
+	$('a[href^="#"]').click(function(event) {
+		event.preventDefault();
+		$root.animate({scrollTop:$( $(this).attr('href')).position().top}, duration);
+		return false;
+	});
+	
+	$('.topReturn').css({ left: '-2em'});
+
+	$(window).scroll(function() {
+		if ($(this).scrollTop() > checkPoint) {
+			$('.topReturn').fadeIn(duration);
+		} else {
+			$('.topReturn').fadeOut(duration);
+		}
+	});
+
+});
+
